76b089ec169a8e66e04eb63d56fd273f20eed2fd
中井彩乃 <nakaiayano@nakaiayano-no-MacBook-Pro.local>
2020-06-29 12:43:35 +0900

    数当てゲーム修正

--- kazuate/src/kazuate.java
+++ kazuate/src/kazuate.java
@@ -4,21 +4,24 @@ public class kazuate {
   public static void main(String[] args) throws Exception {
     int ans = 27; //答え
     int in = 0; //入力される数
-    int dif; //答えと入力数の差
+    int dif, i;
 
     Scanner sc = new Scanner(System.in); //入力の取得用
-    
+    System.out.println("5回まで入力できます");
     //数当て開始
-    while(in != ans){
-      System.out.print("予想した数字を入力：");
+    for(i=0; i<5; i++){
+      System.out.print(i+1+"回目 予想した数字を入力：");
       in = sc.nextInt();
       dif = ans - in; //答えとの差
-      if(dif == 0) break;
+      if(dif == 0){
+        System.out.println("正解しました");
+        break;
+      }
       else if(Math.abs(dif) > 20) System.out.println("20以上離れています");
       System.out.println("不正解です"); 
     }
-    
-    System.out.println("正解しました");
+
+    System.out.println("終了します");
   }
 
 }
